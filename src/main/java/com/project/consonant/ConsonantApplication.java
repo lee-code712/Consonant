593a205fe60b90adec050f0ec9b69130aec0fa8e
unipoint <unipoint@DESKTOP-ODKGP7U>
2022-09-27 10:10:18 +0900

    오라클 계정 및 db 연결정보 추가

--- src/main/java/com/project/consonant/ConsonantApplication.java
+++ src/main/java/com/project/consonant/ConsonantApplication.java
@@ -2,9 +2,8 @@ package com.project.consonant;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
+@SpringBootApplication
 public class ConsonantApplication {
 
 	public static void main(String[] args) {
